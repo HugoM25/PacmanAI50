fba677707e58fe8038a0d96752b46c93b74bce60
HugoM25 <hddm25@gmail.com>
2024-11-28 18:08:03 +0100

    refactor: now models/trainer are separated from main

--- trainer.py
+++ trainer.py
@@ -1,3 +1,27 @@
+
 class Trainer:
+    '''
+    This class is used to define the interface for training the model
+    '''
     def __init__(self):
         pass
+
+    def train(self, num_iterations: int):
+        '''
+        This method is used to train the model
+        '''
+        raise NotImplementedError
+
+    def load_model(self, model_path: str):
+        '''
+        Load the model
+        @param model_path: Path to the model
+        @return: None
+        '''
+        raise NotImplementedError
+
+    def save(self):
+        '''
+        Save the model
+        '''
+        raise NotImplementedError
